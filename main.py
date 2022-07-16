998a90d19b6fd1a344779c5ae4abc2a412e53471
Himanshu Singla <108661887+himanshu-911@users.noreply.github.com>
2022-07-16 21:12:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import cv2                                               #Importing openCV library for computer vision and image processing
-import mediapipe as mp                                   #This library is used to detect the hands
-cap = cv2.VideoCapture(0)                                #This will return video from the first webcam on your computer
-hand_detector = mp.solutions.hands.Hands()               #
-drawing_utils = mp.solutions.drawing_utils               #Used to display hand landmarks
-while True:                                              #To capture frames from video and display video feed
-    _, frame = cap.read()
-    frame = cv2.flip(frame, 1)                           #Used to flip the video
+import cv2                                               
+import mediapipe as mp                                   
+cap = cv2.VideoCapture(0)                               
+hand_detector = mp.solutions.hands.Hands()               
+drawing_utils = mp.solutions.drawing_utils               
+while True:                                              
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)                           
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output = hand_detector.process(rgb_frame)
     hands = output.multi_hand_landmarks
